9e64a236b6fd1adc5d994de6fdae74eba2e5aa0a
Oskar Kaczmarzyk <oskar.kaczmarzyk@icloud.com>
2024-07-03 14:52:00 +0200

    feat: implement fetching group forecast for given locations
    * added useGetGroupForecast, 
    * added Weather type & subtypes, 
    * added LOCATION_CODES, 
    * added mapLocationCodes helper.
    * fixed useGetGroupForecast return

--- App.tsx
+++ App.tsx
@@ -9,24 +9,28 @@ import React, {FC} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Details, Home} from './src/screens';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 const Stack = createNativeStackNavigator();
 
 export const App: FC = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={Home}
-          options={{title: 'Weather'}}
-        />
-        <Stack.Screen
-          name="Details"
-          component={Details}
-          options={{title: 'Details', headerBackTitle: 'Back'}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{title: 'Weather'}}
+          />
+          <Stack.Screen
+            name="Details"
+            component={Details}
+            options={{title: 'Details', headerBackTitle: 'Back'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 };
---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export {useGetGroupForecast} from './useGetGroupForecast';

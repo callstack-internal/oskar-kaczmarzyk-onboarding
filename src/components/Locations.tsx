4de88dcb422ee3ebe22414404f02918adffc6e49
Oskar Kaczmarzyk <oskar.kaczmarzyk@icloud.com>
2024-07-04 14:31:30 +0200

    chore: added refetching, added Error component

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -1,23 +1,43 @@
 import React, {FC} from 'react';
-import {FlatList} from 'react-native';
+import {FlatList, Text} from 'react-native';
 import {LocationItem} from './LocationItem';
 import {Weather} from '../types';
 import {useNavigation} from '@react-navigation/native';
+import {useGetGroupForecast} from '../hooks';
 
 type LocationsProps = {
   data: Weather[];
 };
 
 export const Locations: FC<LocationsProps> = ({data}) => {
+  const {isLoading, refetch} = useGetGroupForecast();
   const {navigate} = useNavigation();
+
+  const handleRefresh = () => {
+    refetch();
+  };
+
+  const handleDetailsNavigation = (id: number) => {
+    navigate('Details', {id});
+  };
+
+  const getEmptyState = () => {
+    if (isLoading) {
+      return null;
+    }
+    return <Text>No forecasts to be shown.</Text>;
+  };
 
   return (
     <FlatList
+      refreshing={isLoading}
+      onRefresh={handleRefresh}
       data={data}
+      ListEmptyComponent={getEmptyState()}
       renderItem={({item}) => (
         <LocationItem
           weather={item}
-          onPress={() => navigate('Details', {id: item.id})}
+          onPress={() => handleDetailsNavigation(item.id)}
         />
       )}
     />
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export {Locations} from './Locations';
+export {LocationItem} from './LocationItem';
 export {DetailsRow} from './DetailsRow';
+export {Error} from './Error';
